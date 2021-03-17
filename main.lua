5618dbe08556b17383c8a2567158bdcd3fafbd50
GoogleFeud <volen.sl666@gmail.com>
2021-03-17 16:46:50 +0200

    Connection to TCP server

--- main.lua
+++ main.lua
@@ -1,11 +1,34 @@
-local DiscordTBOA = RegisterMod("DiscordTBOA", 1);
--- local Socket = require("socket");
+require("constants")
+require("config")
+local DiscordTBOI = RegisterMod("DiscordTBOI", 1)
 
-function DiscordTBOA:Immortality(_mod)
+local Socket = require("socket")
+local tcp = Socket.tcp()
+tcp:settimeout(0.0001)
+tcp:connect(HOST, PORT)
+Isaac.ConsoleOutput("DiscordTBOI: Connected to server")
+
+local rng = RNG()
+rng:SetSeed(os.time(), 1)
+
+local userId = rng:Next()
+
+tcp:send(PACKET_SEND_CODES["AUTH"] .. userId)
+Isaac.ConsoleOutput("DiscordTBOI: Your ID is" .. userId)
+
+function DiscordTBOI:Immortality(_mod)
     local player = Isaac.GetPlayer(0)
     player:SetFullHearts()
 end
 
+DiscordTBOI:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, DiscordTBOI.Immortality, EntityType.ENTITY_PLAYER)
 
-DiscordTBOA:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, DiscordTBOA.Immortality, EntityType.ENTITY_PLAYER)
+function DiscordTBOI:EventListener(_mod)
+    local packet, err = tcp:receive()
+    if packet ~= nil then 
+        Isaac.ExecuteCommand(packet)
+    end
+end
+
+DiscordTBOI:AddCallback(ModCallbacks.MC_POST_UPDATE, DiscordTBOI.EventListener)
 
